58b61864ea0a563b566828b66ba8af805fca8043
Daniel Hogan <djhogan87@gmail.com>
2025-03-27 18:41:46 -0500

    [1.19a] Display characters created in 1981 (all series)

--- Program.cs
+++ Program.cs
@@ -93,11 +93,14 @@ Console.Clear();
 // }
 
 // all characters grouped by year created
-var CharactersByYearCreated = characters.GroupBy(c => c.YearCreated);
-foreach(var characterByYearCreated in CharactersByYearCreated)
-{
-  Console.WriteLine(characterByYearCreated.Key);
-  foreach(var character in characterByYearCreated) {
-    Console.WriteLine($"\t{character.Name}");
-  }
-}
+// var CharactersByYearCreated = characters.GroupBy(c => c.YearCreated);
+// foreach(var characterByYearCreated in CharactersByYearCreated)
+// {
+//   Console.WriteLine(characterByYearCreated.Key);
+//   foreach(var character in characterByYearCreated) {
+//     Console.WriteLine($"\t{character.Name}");
+//   }
+// }
+
+// Display characters created in 1981 (all series)
+Console.WriteLine($"Characters created in 1981 - {characters.Count(c => c.YearCreated == 1981)}");
